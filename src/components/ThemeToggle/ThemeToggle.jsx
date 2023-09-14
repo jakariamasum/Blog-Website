4cf307b5e041620dd59584c377432b1479fb88aa
jakariamasum <jmphy698@gmail.com>
2023-09-14 19:37:21 +0600

    navbar added

---
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './ThemeToggole.css'
+const ThemeToggle = () => {
+    return (
+        <div className={styles.container}>
+            ThemeToggle
+        </div>
+    );
+};
+
+export default ThemeToggle;
---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './AuthLinks.css'
+const AuthLinks = () => {
+    return (
+        <div className={styles.container}>
+            AuthLinks
+        </div>
+    );
+};
+
+export default AuthLinks;
