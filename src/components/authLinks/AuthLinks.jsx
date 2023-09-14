6c07b4675936fc1d22d74cc138473af3651f8d75
jakariamasum <jmphy698@gmail.com>
2023-09-15 00:07:37 +0600

    make navbar mobile responsive

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import styles from './AuthLinks.module.css'
 import Link from 'next/link';
 const AuthLinks = () => {
-    const isLogin=false;
+    const isLogin = false;
+    const [open, setOpen] = useState(false);
     return (
         <div>
-            {
-                isLogin?<>
-                <Link href='/write'>Write</Link>
-                <span className={styles.link}>LogOut</span>
-                </>:<Link href='/login'>Login</Link>
-            }
+            {isLogin ? (
+                <Link href="/login" className={styles.link}>
+                    Login
+                </Link>
+            ) : (
+                <>
+                    <Link href="/write" className={styles.link}>
+                        Write
+                    </Link>
+                    <span className={styles.link}>
+                        Logout
+                    </span>
+                </>
+            )}
+            <div className={styles.burger} onClick={() => setOpen(!open)}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            {open && (
+                <div className={styles.responsiveMenu}>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">About</Link>
+                    <Link href="/">Contact</Link>
+                    {isLogin ? (
+                        <Link href="/login">Login</Link>
+                    ) : (
+                        <>
+                            <Link href="/write">Write</Link>
+                            <span className={styles.link}>Logout</span>
+                        </>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
