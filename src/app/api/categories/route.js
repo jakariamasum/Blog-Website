92e76a56bc7cd585a67ce07a650770d4db6f779d
jakariamasum <jmphy698@gmail.com>
2023-10-01 23:12:33 +0600

    problem identify

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -2,12 +2,13 @@ import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
+  console.log('trying..............')
   try {
     const categories = await prisma.category.findMany();
 
     return new NextResponse(JSON.stringify(categories, { status: 200 }));
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
     return new NextResponse(
       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
     );
